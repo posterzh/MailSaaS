c04153d109b133746bc8ecbe0fcd64a5b43ad057
Zhang.P <poterasu.zhang@gmail.com>
2021-01-25 18:26:12 +0530

    added api page code

--- assets/javascript/src/views/pages/Campaing/Campaign.js
+++ assets/javascript/src/views/pages/Campaing/Campaign.js
@@ -1,83 +1,115 @@
 import React, { Component } from 'react'
-import Container from 'reactstrap/lib/Container'
+import { Container, Row, Col, Label, Input, Dropdown, DropdownToggle, DropdownMenu, DropdownItem, Card, Table } from 'reactstrap'
 
-// importing reacstrap elements
-import {
-  Badge,
-  Button,
-  Card,
-  CardHeader,
-  CardBody,
-  DropdownMenu,
-  DropdownItem,
-  DropdownToggle,
-  UncontrolledDropdown,
-  Form,
-  Input,
-  ListGroupItem,
-  ListGroup,
-  Media,
-  NavItem,
-  NavLink,
-  Nav,
-  Progress,
-  Table,
-  // Container,
-  Row,
-  Col,
-  UncontrolledTooltip
-} from "reactstrap";
-
-
-
-
-export class Campaign extends Component {
+class Campaign extends Component {
+  constructor() {
+    super();
+    this.state = {
+      dd1: false,
+      value: 'Any'
+    };
+  }
+  dropdownToggle = () => {
+    this.setState({
+      dd1: !this.state.dd1
+    });
+  }
+  select = (e) => {
+    this.setState({
+      dropdownOpen: !this.state.dropdownOpen,
+      value: event.target.innerText
+    });
+  }
   render() {
     return (
-<div>   
-  <div>
-    <Input
-    style={{width:"400px"}}
-    placeholder="search"
-    />
-    </div>
- <div class="Campaing_table_container" style={{marginleft:"20px"}}>
-  <Card>  
-<table class="table">
-  <thead>
-    <tr>
-      <th scope="col" className="tableheader1" ><input type="checkbox"/></th>
-      <th scope="col" className="tableheader2">Campaign Title</th>
-      <th scope="col" className="header_created">Created</th>
-      <th scope="col" className="header_assigned">Assigned</th>
-      <th scope="col" className="header_recipents">RECIPIENTS</th>
-      <th scope="col" className="header_sent">SENT</th>
-      <th scope="col" className="header_leads">LEADS</th>
-      <th scope="col" className="header_replies">REPLIES</th>
-      <th scope="col" className="header_open">OPENS</th>
-      <th scope="col" className="header_bounces">BOUNCES</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td className="check_box"><input type="checkbox"/></td>
-      <td className="Campaign_title">January 19 Outreach</td>
-      <td className="Created">0</td>
-      <td className="Assigned">0</td>
-      <td className="Recipient">0</td>
-      <td className="Sent">0</td>
-      <td className="Leads" >0</td>
-      <td className="Replies">0</td>
-      <td className="Open">0</td>
-      <td className="Bounces">0</td>
-    </tr>
-  </tbody>
-</table>
-</Card>
-</div>
-  </div>
+      <div>
+        <div className="Campaign_barcontainer">
+          <Container fluid className='mt-0'>
+
+            <Row>
+            <Col md='4' className=' '>
+                            <div className='grand_parent'>
+                                <div className='input_field'>
+                                    <Input type='email' className='in' placeholder='Search' />
+                                    <div className='child'>
+                                        <a href='' onClick={(e) => { e.preventDefault(); alert('msg') }}>
+                                            <span className='font_icon'><i class="fa fa-search" aria-hidden="true"></i></span>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </Col>
+              <Col md='4'>
+              <div>
+                        <label className='filter_app'>Filter by app</label><br/>
+                        <select className='filter_select'>
+                            <option value='one'>any</option>
+                            <option value='two'>unassigned</option>
+                            <option value='three'>omaid faziyer</option>
+                           
+                        </select>
+                    </div>
+                
+                {/* <Dropdown isOpen={this.state.dd1} toggle={this.dropdownToggle} >
+                  <DropdownToggle caret className='drop_span' >
+                    <span onClick={this.dropdownToggle}>
+                      {this.state.value}
+                    </span>
+                  </DropdownToggle>
+                  <DropdownMenu>
+                    <DropdownItem onClick={this.select}>Header</DropdownItem>
+                    <DropdownItem onClick={this.select}>Action</DropdownItem>
+                  </DropdownMenu>
+                </Dropdown> */}
+              </Col>
+            </Row>
+
+          </Container>
+        </div>
+        <Container fluid className='mt-4' >
+
+          <Card>
+            <Row>
+              <Col md='12'>
+                <Table responsive hover >
+                  <thead>
+                    <tr>
+                      <th scope="col" className="tableheader1" ><input type="checkbox" /></th>
+                      <th className="tableheader2">Campaign Title</th>
+                      <th className="header_created">Created</th>
+                      <th className="header_assigned">Assigned</th>
+                      <th className="header_recipents">RECIPIENTS</th>
+                      <th className="header_sent">SENT</th>
+                      <th className="header_leads">LEADS</th>
+                      <th className="header_replies">REPLIES</th>
+                      <th className="header_open">OPENS</th>
+                      <th className="header_bounces">BOUNCES</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td><input type='checkbox' /></td>
+                      <td className="Campaign_title">January 19 Outreach</td>
+                      <td className="Created">0</td>
+                      <td className="Assigned">0</td>
+                      <td className="Recipient">0</td>
+                      <td className="Sent">0</td>
+                      <td className="Leads" >0</td>
+                      <td className="Replies">0</td>
+                      <td className="Open">0</td>
+                      <td className="Bounces">0</td>
+                    </tr>
+                  </tbody>
+                </Table>
+              </Col>
+            </Row>
+          </Card>
+        </Container>
+
+      </div>
     )
   }
 }
+
 
 export default Campaign
--- assets/javascript/src/views/pages/Campaing/LeadCatcher.js
+++ assets/javascript/src/views/pages/Campaing/LeadCatcher.js
@@ -1,123 +1,160 @@
 import React, { Component } from 'react'
 // importing reacstrap elements
-import {
-  Badge,
-  Button,
-  Card,
-  CardHeader,
-  CardBody,
-  UncontrolledDropdown,
-  Form,
-  Input,
-  ListGroupItem,
-  ListGroup,
-  Media,
-  NavItem,
-  NavLink,
-  Nav,
-  Progress,
-  Table,
-  // Container,
-  Row,
-  Col,
-  UncontrolledTooltip
-} from "reactstrap";
-import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+import { Container, Row, Col, Label, Input, Dropdown, DropdownToggle, DropdownMenu, DropdownItem, Table, Button, Card } from 'reactstrap'
 
 class LeadCatcher extends Component {
 
-  constructor(){
+  constructor() {
     super();
     this.state = {
-      dropdownOpen: false
-    }
+      dd1: false,
+      value: 'Any'
+    };
   }
-
-  toggle = () => {
+  dropdownToggle = () => {
     this.setState({
-      dropdownOpen: !this.state.dropdownOpen
-    })
+      dd1: !this.state.dd1
+    });
   }
-
+  select = (e) => {
+    this.setState({
+      dropdownOpen: !this.state.dropdownOpen,
+      value: event.target.innerText
+    });
+  }
   render() {
-
-    const {dropdownOpen} = this.state;
     return (
-    <div >
-      <div className="Barcontainer">  
-        <div>
-          <Input  className="searchbar" type='text' placeholder='search'></Input>
+      <div >
+        <div className="graph_container" style={{ display: "flex", flexDirection: "row-reverse" }}>
+          <span className="graph_title">Last 30 days</span>
         </div>
-        <div>
-        <Dropdown isOpen={dropdownOpen} toggle={this.toggle}>
-      <DropdownToggle className="dropdown_campaign" caret>
-        Dropdown
-      </DropdownToggle>
-      <DropdownMenu>
-        <DropdownItem>Header</DropdownItem>
-        <DropdownItem>Some Action</DropdownItem>
-        <DropdownItem text>Dropdown Item Text</DropdownItem>
-        <DropdownItem>Action (disabled)</DropdownItem>
-      </DropdownMenu>
-    </Dropdown>
+
+        <div className="Leadcatcher_Barcontainer">
+
+          <Container style={{ pading: "20px 30px 0px" }} fluid className='mt-0'>
+            <Row>
+              <Col md="4">
+                <div>
+                  <Col md='4' className=' '>
+                    <div className='grand_parent'>
+                      <div className='input_field'>
+                        <Input type='email' className='in' placeholder='Search' />
+                        <div className='child'>
+                          <a href='' onClick={(e) => { e.preventDefault(); alert('msg') }}>
+                            <span className='font_icon'><i class="fa fa-search" aria-hidden="true"></i></span>
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+                  </Col>
+                </div>
+              </Col>
+              <Col md="2">
+              <div>
+                        
+                        <select className='filter_select'>
+                            <option value='one'>All Campaigns</option>
+                            <option value='two'>Campaign 1</option>
+                            <option value='three'>Campaign 2</option>
+                           
+                        </select>
+                    </div>
+                {/* <Dropdown tabIndex={0} isOpen={this.state.dd1} toggle={this.dropdownToggle} >
+                  <DropdownToggle caret className='drop_span' >
+                    <span onClick={this.dropdownToggle}>
+                      {this.state.value}
+                    </span>
+                  </DropdownToggle>
+                  <DropdownMenu>
+                    <DropdownItem onClick={this.select}>Header</DropdownItem>
+                    <DropdownItem onClick={this.select}>Action</DropdownItem>
+                  </DropdownMenu>
+                </Dropdown> */}
+              </Col>
+              <Col md="2">
+              <div>
+                        
+                        <select className='filter_select'>
+                            <option value='one'>unassigned</option>
+                            <option value='two'>unassigned1</option>
+                            <option value='three'>unassigned2</option>
+                           
+                        </select>
+                    </div>
+                {/* <Dropdown isOpen={this.state.dd1} toggle={this.dropdownToggle} >
+                  <DropdownToggle caret className='drop_span' >
+                    <span onClick={this.dropdownToggle}>
+                      {this.state.value}
+                    </span>
+                  </DropdownToggle>
+                  <DropdownMenu>
+                    <DropdownItem onClick={this.select}>Header</DropdownItem>
+                    <DropdownItem onClick={this.select}>Action</DropdownItem>
+                  </DropdownMenu>
+                </Dropdown> */}
+              </Col>
+              <Col md="2">
+              <div>
+                        
+                        <select className='filter_select'>
+                            <option  value='open'>open</option>
+                            <option  value='lost'>lost</option>
+                            <option value='won'>won</option>
+                            <option value='ignore'>ignore</option>
+                           
+                        </select>
+                    </div>
+                {/* <Dropdown isOpen={this.state.dd1} toggle={this.dropdownToggle} >
+                  <DropdownToggle caret className='drop_span' >
+                    <span onClick={this.dropdownToggle}>
+                      {this.state.value}
+                    </span>
+                  </DropdownToggle>
+                  <DropdownMenu>
+                    <DropdownItem onClick={this.select}>Header</DropdownItem>
+                    <DropdownItem onClick={this.select}>Action</DropdownItem>
+                  </DropdownMenu>
+                </Dropdown> */}
+              </Col>
+              <Col md="2">
+                <div className='refresh_child mt-0'>
+                  <a href='' onClick={(e) => { e.preventDefault(); alert('msg') }}>
+                    <span className='font_icon'><i class="fa fa-undo" aria-hidden="true"></i></span>
+                  </a>
+                </div>
+
+              </Col>
+            </Row>
+          </Container>
         </div>
-        <div>
-        <Dropdown isOpen={dropdownOpen} toggle={this.toggle}>
-      <DropdownToggle caret>
-        Dropdown
-      </DropdownToggle>
-      <DropdownMenu>
-        <DropdownItem>Header</DropdownItem>
-        <DropdownItem>Some Action</DropdownItem>
-        <DropdownItem text>Dropdown Item Text</DropdownItem>
-        <DropdownItem>Action (disabled)</DropdownItem>
-      </DropdownMenu>
-    </Dropdown>
-        </div>
-        <div>
-        <Dropdown isOpen={dropdownOpen} toggle={this.toggle}>
-      <DropdownToggle caret>
-        Dropdown
-      </DropdownToggle>
-      <DropdownMenu>
-        <DropdownItem>Header</DropdownItem>
-        <DropdownItem>Some Action</DropdownItem>
-        <DropdownItem text>Dropdown Item Text</DropdownItem>
-        <DropdownItem>Action (disabled)</DropdownItem>
-      </DropdownMenu>
-    </Dropdown>
-        </div>
-        <div>
-          <Button />
+
+        <div className="Leadcatcher_table">
+          <Card className="table">
+            <table >
+              <thead>
+                <tr>
+                  <th scope="col" className="tableheader1" ><input type="checkbox" /></th>
+                  <th scope="col" className="tableheader2">Person</th>
+                  <th scope="col" className="header_created">Campaign</th>
+                  <th scope="col" className="header_assigned">AssignedTo</th>
+                  <th scope="col" className="header_recipents">LeadDate</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td className="check_box"><input type="checkbox" /></td>
+                  <td className="Campaign_title">Person name</td>
+                  <td className="Created">Campaign name</td>
+                  <td className="Assigned">Omaid Faizyar</td>
+                  <td className="Recipient">00:00</td>
+                </tr>
+              </tbody>
+            </table>
+          </Card>
         </div>
       </div>
-      <div>
-        <Card className="table">
-          <table >
-            <thead>
-              <tr>
-                <th scope="col" className="tableheader1" ><input type="checkbox" /></th>
-                <th scope="col" className="tableheader2">Person</th>
-                <th scope="col" className="header_created">Campaign</th>
-                <th scope="col" className="header_assigned">AssignedTo</th>
-                <th scope="col" className="header_recipents">LeadDate</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td className="check_box"><input type="checkbox" /></td>
-                <td className="Campaign_title">Person name</td>
-                <td className="Created">Campaign name</td>
-                <td className="Assigned">Omaid Faizyar</td>
-                <td className="Recipient">00:00</td>
-              </tr>
-            </tbody>
-          </table>
-        </Card>
-      </div>
-    </div>
-  )
-}
+    )
+  }
 }
 
 export default LeadCatcher
