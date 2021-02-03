33167d8f847aa292f339e3f87a563b0a931124c1
Zhang.P <poterasu.zhang@gmail.com>
2021-02-03 15:43:50 +0530

    change login signup

--- apps/users/serializer.py
+++ apps/users/serializer.py
@@ -1,22 +1,7 @@
-from rest_framework import serializers,exceptions
+from rest_framework import serializers
 from .models import CustomUser 
 from rest_framework.validators import UniqueValidator
-from django.conf import settings
-from django.contrib.auth import authenticate,get_user_model
 
-from django.utils.translation import ugettext_lazy as _
-
-try:
-    from allauth.account import app_settings as allauth_settings
-    from allauth.utils import (email_address_exists)
-    from allauth.account.adapter import get_adapter
-    from allauth.account.utils import setup_user_email
-
-except ImportError:
-    raise ImportError("allauth needs to be added to INSTALLED_APPS.")
-
-
-UserModel = get_user_model()
 
 class RegisterSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(
@@ -26,14 +11,13 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = CustomUser
-        fields = ('id','password','email','full_name','company_name','phone_number','mailsaas_type','avatar')
+        fields = ('id','password','email','full_name','company_name','phone_number','avatar')
         extra_kwargs = {
             
             'email': {'required': True},
             'company_name': {'required': True},
             'full_name': {'required': True},
             'phone_number': {'required': True},
-            'mailsaas_type': {'required': True},
             'password': {'required': True},
             
         }
@@ -50,140 +34,18 @@ class RegisterSerializer(serializers.ModelSerializer):
         )
         user.save()
         return user
-    
-    def validate_username(self, username):
-        username = get_adapter().clean_username(username)
-        return username
 
-    def validate_email(self, email):
-        email = get_adapter().clean_email(email)
-        if allauth_settings.UNIQUE_EMAIL:
-            if email and email_address_exists(email):
-                raise serializers.ValidationError(
-                    _("A user is already registered with this e-mail address."))
-        return email
 
-    def validate_password1(self, password):
-        return get_adapter().clean_password(password)
+class LoginSerilizer(serializers.ModelSerializer):
 
-    def validate(self, data):
-        # if data['password1'] != data['password2']:
-        #     raise serializers.ValidationError(_("The two password fields didn't match."))
-        return data
-
-    def custom_signup(self, request, user):
-        pass
-
-    def get_cleaned_data(self):
-        return {
-            # 'username': self.validated_data.get('username', ''),
-            # 'password1': self.validated_data.get('password1', ''),
-            'email': self.validated_data.get('email', '')
+    class Meta:
+        model = CustomUser
+        fields = ['email','password']
+        extra_kwargs = {
+            'email': {'required': True},
+            'password': {'required': True},
         }
 
-    def save(self, request):
-        adapter = get_adapter()
-        user = adapter.new_user(request)
-        self.cleaned_data = self.get_cleaned_data()
-        adapter.save_user(request, user, self)
-        self.custom_signup(request, user)
-        setup_user_email(request, user, [])
-        return user
-class LoginSerializer(serializers.Serializer):
-    # username = serializers.CharField(required=False, allow_blank=True)
-    email = serializers.EmailField(required=False, allow_blank=True)
-    password = serializers.CharField(style={'input_type': 'password'})
-
-    def authenticate(self, **kwargs):
-        return authenticate(self.context['request'], **kwargs)
-
-    def _validate_email(self, email, password):
-        user = None
-
-        if email and password:
-            user = self.authenticate(email=email, password=password)
-        else:
-            msg = _('Must include "email" and "password".')
-            raise exceptions.ValidationError(msg)
-
-        return user
-
-    def _validate_username(self, username, password):
-        user = None
-
-        if username and password:
-            user = self.authenticate(username=username, password=password)
-        else:
-            msg = _('Must include "username" and "password".')
-            raise exceptions.ValidationError(msg)
-
-        return user
-
-    def _validate_username_email(self, username, email, password):
-        user = None
-
-        if email and password:
-            user = self.authenticate(email=email, password=password)
-        elif username and password:
-            user = self.authenticate(username=username, password=password)
-        else:
-            msg = _('Must include either "username" or "email" and "password".')
-            raise exceptions.ValidationError(msg)
-
-        return user
-
-    def validate(self, attrs):
-        username = attrs.get('username')
-        email = attrs.get('email')
-        password = attrs.get('password')
-
-        user = None
-
-        if 'allauth' in settings.INSTALLED_APPS:
-            from allauth.account import app_settings
-
-            # Authentication through email
-            if app_settings.AUTHENTICATION_METHOD == app_settings.AuthenticationMethod.EMAIL:
-                user = self._validate_email(email, password)
-
-            # Authentication through username
-            elif app_settings.AUTHENTICATION_METHOD == app_settings.AuthenticationMethod.USERNAME:
-                user = self._validate_username(username, password)
-
-            # Authentication through either username or email
-            else:
-                user = self._validate_username_email(username, email, password)
-
-        else:
-            # Authentication without using allauth
-            if email:
-                try:
-                    username = UserModel.objects.get(email__iexact=email).get_username()
-                except UserModel.DoesNotExist:
-                    pass
-
-            if username:
-                user = self._validate_username_email(username, '', password)
-
-        # Did we get back an active user?
-        if user:
-            if not user.is_active:
-                msg = _('User account is disabled.')
-                raise exceptions.ValidationError(msg)
-        else:
-            msg = _('Unable to log in with provided credentials.')
-            raise exceptions.ValidationError(msg)
-
-        # If required, is the email verified?
-        if 'rest_auth.registration' in settings.INSTALLED_APPS:
-            from allauth.account import app_settings
-            if app_settings.EMAIL_VERIFICATION == app_settings.EmailVerificationMethod.MANDATORY:
-                email_address = user.emailaddress_set.get(email=user.email)
-                if not email_address.verified:
-                    raise serializers.ValidationError(_('E-mail is not verified.'))
-
-        attrs['user'] = user
-        return attrs
 
 class TokenSerializer(serializers.Serializer):
     
@@ -205,23 +67,3 @@ class ChangePasswordSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = ['old_password','new_password','new_confirm_password']
-
-
-class JWTSerializer(serializers.Serializer):
-    """
-    Serializer for JWT authentication.
-    """
-    token = serializers.CharField()
-    user = serializers.SerializerMethodField()
-
-    def get_user(self, obj):
-        """
-        Required to allow using custom USER_DETAILS_SERIALIZER in
-        JWTSerializer. Defining it here to avoid circular imports
-        """
-        rest_auth_serializers = getattr(settings, 'REST_AUTH_SERIALIZERS', {})
-        JWTUserDetailsSerializer = import_callable(
-            rest_auth_serializers.get('USER_DETAILS_SERIALIZER', UserDetailsSerializer)
-        )
-        user_data = JWTUserDetailsSerializer(obj['user'], context=self.context).data
-        return user_data
