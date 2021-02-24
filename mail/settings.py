6d00ea77f82ddf326aca2a273336ef202253abae
Zhang.P <poterasu.zhang@gmail.com>
2021-02-24 14:35:36 +0530

    docker devlop

--- mail/settings.py
+++ mail/settings.py
@@ -151,7 +151,7 @@ DATABASES = {
         'NAME': config('DATABASE_NAME'),
         'USER': config('DATABASE_USER'),
         'PASSWORD': config('DATABASE_PASSWORD'),
-        'HOST': config('DATABASE_HOST'),
+        'HOST':config('DATABASE_HOST'),
         'PORT':config('DATABASE_PORT'),
     }
 }
