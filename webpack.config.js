5fce50c0d16bc646fe34cfb8f4703a766ee77594
Zhang.P <poterasu.zhang@gmail.com>
2021-02-27 19:26:20 +0800

    Add debug configuration.

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,16 @@
-const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: {
-    site: './assets/site.js',  // required for bulma sass/css styles
-    app: './assets/javascript/app.js',
-    teams: './assets/javascript/teams.js',
-    'react-object-lifecycle': './assets/javascript/src/index.js',
+    site: "./assets/site.js", // required for bulma sass/css styles
+    app: "./assets/javascript/app.js",
+    teams: "./assets/javascript/teams.js",
+    "react-object-lifecycle": "./assets/javascript/src/index.js",
   },
   output: {
-    path: path.resolve(__dirname, './static'),
-    filename: 'js/[name]-bundle.js',
+    path: path.resolve(__dirname, "./static"),
+    filename: "js/[name]-bundle.js",
     library: ["SiteJS", "[name]"],
   },
   module: {
@@ -19,21 +19,20 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         loader: "babel-loader",
-        options: { presets: ["@babel/env", "@babel/preset-react"] }
+        options: { presets: ["@babel/env", "@babel/preset-react"] },
       },
       {
         test: /\.(css|scss)$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          'sass-loader',
-        ],
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
       },
-    ]
+    ],
   },
   plugins: [
     new MiniCssExtractPlugin({
-      'filename': 'css/[name].css',
+      filename: "css/[name].css",
     }),
-  ]
+  ],
+  devServer: {
+    writeToDisk: true,
+  },
 };
