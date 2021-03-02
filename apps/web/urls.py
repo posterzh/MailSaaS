cc10f48e60343814625bd26917e1f20b335b402b
Zhang.P <poterasu.zhang@gmail.com>
2021-03-02 10:57:54 -0800

    terms and privacy

--- apps/web/urls.py
+++ apps/web/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
 
     path(r'a/<slug:team_slug>', views.team_home, name='team_home'),
     path(r'a/<slug:team_slug>/manage/', views.team_admin_home, name='team_admin'),
-
+    path(r'privacy', TemplateView.as_view(template_name="web/privacy.html"), name='privacy'),
     path(r'terms', TemplateView.as_view(template_name="web/terms.html"), name='terms'),
 ]
