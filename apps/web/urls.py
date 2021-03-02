1d4dca3365439beb5779919aa77c7efb89ddf3d1
Zhang.P <poterasu.zhang@gmail.com>
2021-03-02 14:35:45 -0800

    verify

--- apps/web/urls.py
+++ apps/web/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path(r'a/<slug:team_slug>/manage/', views.team_admin_home, name='team_admin'),
     path(r'privacy', TemplateView.as_view(template_name="web/privacy.html"), name='privacy'),
     path(r'terms', TemplateView.as_view(template_name="web/terms.html"), name='terms'),
+    path('.well-known/microsoft-identity-association.json', views.msfile),
 ]
