917dbf4435ba061d51a1c95b08c94c9723c786da
Zhang.P <poterasu.zhang@gmail.com>
2021-03-03 01:11:49 +0800

    Convert New Campaign page to use Argon theme.

--- assets/javascript/src/views/pages/campaign/NewCampaign/CampaignStart.js
+++ assets/javascript/src/views/pages/campaign/NewCampaign/CampaignStart.js
@@ -9,6 +9,7 @@ import {
   Input,
   Button,
 } from "reactstrap";
+import { Link } from "react-router-dom";
 import { StartCampaignAction } from "../../../../redux/action/CampaignAction";
 import { MailGetDataAction } from "../../../../redux/action/MailSenderAction";
 import PageHeader from "../../../../components/Headers/PageHeader";
@@ -98,76 +99,88 @@ class CampaignStart extends React.Component {
         />
 
         <PageContainer title="New Campaign">
-          <Form onSubmit={this.handleSubmit}>
-            <Row>
-              <Col>
-                <CampaignsHeader active="START" />
-              </Col>
-            </Row>
-            <Row>
-              <Col>
-                <h1 className="text-center my-4">Let's get started</h1>
-              </Col>
-            </Row>
+          <Row>
+            <Col md={8} className="mx-auto">
+              <Form onSubmit={this.handleSubmit}>
+                <Row>
+                  <Col>
+                    <CampaignsHeader active="START" />
+                  </Col>
+                </Row>
+                <Row>
+                  <Col>
+                    <h1 className="text-center my-4">Let's get started</h1>
+                  </Col>
+                </Row>
 
-            <Row>
-              <Col>
-                <FormGroup>
-                  <label className="form-control-label" htmlFor="inputTitle">
-                    Title
-                  </label>
-                  <Input
-                    id="inputTitle"
-                    type="text"
-                    name="title"
-                    value={this.state.title}
-                    onChange={this.handleChange}
-                    placeholder={this.state.date}
-                  />
-                </FormGroup>
-                <FormGroup>
-                  <label
-                    className="form-control-label"
-                    htmlFor="selectFromAddress"
-                  >
-                    From Address
-                  </label>
-                  <Input
-                    id="selectFromAddress"
-                    type="select"
-                    name="from_address"
-                    value={this.state.from_address}
-                    onChange={this.handleChange}
-                  >
-                    <option value={""}>Select</option>
-                    {mailGetData &&
-                      mailGetData.map((item, index) => {
-                        return (
-                          <option key={index} value={item.id}>
-                            {item.email}
-                          </option>
-                        );
-                      })}
-                  </Input>
-                </FormGroup>
-              </Col>
-            </Row>
+                <Row>
+                  <Col>
+                    <FormGroup>
+                      <label
+                        className="form-control-label"
+                        htmlFor="inputTitle"
+                      >
+                        Title
+                      </label>
+                      <Input
+                        id="inputTitle"
+                        type="text"
+                        name="title"
+                        value={this.state.title}
+                        onChange={this.handleChange}
+                        placeholder={this.state.date}
+                      />
+                    </FormGroup>
+                    <FormGroup>
+                      <label
+                        className="form-control-label"
+                        htmlFor="selectFromAddress"
+                      >
+                        From Address
+                      </label>
+                      <Input
+                        id="selectFromAddress"
+                        type="select"
+                        name="from_address"
+                        value={this.state.from_address}
+                        onChange={this.handleChange}
+                      >
+                        <option value={""}>Select</option>
+                        {mailGetData &&
+                          mailGetData.map((item, index) => {
+                            return (
+                              <option key={index} value={item.id}>
+                                {item.email}
+                              </option>
+                            );
+                          })}
+                      </Input>
+                    </FormGroup>
+                  </Col>
+                </Row>
 
-            <Row>
-              <Col>
-                <Button
-                  color="primary"
-                  type="button"
-                  type="submit"
-                  disabled={!mailsExist}
-                  className="my-3"
-                >
-                  NEXT&nbsp;
-                  <i className="fa fa-arrow-right" aria-hidden="true"></i>
-                </Button>
-              </Col>
-            </Row>
-          </Form>
+                <Row className="my-3">
+                  <Col className="d-flex align-items-center justify-content-center">
+                    <Link
+                      to={{
+                        pathname: "/app/admin/CampaignRecipient",
+                        state: {
+                          id:
+                            this.props.history.location.state &&
+                            this.props.history.location.state.id,
+                        },
+                      }}
+                    >
+                      <Button color="danger" type="button" type="submit">
+                        NEXT{" "}
+                        <i className="fa fa-arrow-right" aria-hidden="true"></i>
+                      </Button>
+                    </Link>
+                  </Col>
+                </Row>
+              </Form>
+            </Col>
+          </Row>
         </PageContainer>
       </>
     );
