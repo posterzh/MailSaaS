804c1a48eb4bdc6668069529832c5b74267aacc4
Zhang.P <poterasu.zhang@gmail.com>
2021-03-04 08:02:00 +0800

    Create the MailAccount Model.

--- apps/mailaccounts/models.py
+++ apps/mailaccounts/models.py
@@ -2,42 +2,82 @@ from django.db import models
 
 from apps.users.models import CustomUser
 
-# Create your models here.
-
-Smtp_Port = (
-    ("587",'587'),
-    ("25" , "25"),
-    ("465","465"),
-    ("2525","2525"),
-)
-Imap_Port = (
-    ("993",'993'),
-    ("143" , "143"),
-    ("995","995"),
+SMTP_PORTS = (
+    ("587", '587'),
+    ("25", "25"),
+    ("465", "465"),
+    ("2525", "2525"),
 )
 
-Provider = (
-    ("SMTP","Smtp"),
-    ("GOOGLE","Google"),
-    ("MICROSOFT","Microsoft"),
+IMAP_PORTS = (
+    ("993", '993'),
+    ("143", "143"),
+    ("995", "995"),
+)
+
+EMAIL_PROVIDERS = (
+    ("SMTP", "SMTP"),
+    ("Google", "Google"),
+    ("Microsoft", "Microsoft"),
 )
 
 
 class EmailAccount(models.Model):
-    user = models.ForeignKey(CustomUser,on_delete=models.CASCADE)
+    # Common fields
+    email_provider = models.CharField(max_length=100, choices=EMAIL_PROVIDERS, default='Smtp', null=True, blank=True)
+
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     email = models.EmailField(unique=True)
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
-    smtp_host = models.CharField(max_length=200,blank=True,null = True)
-    smtp_username = models.CharField(max_length=200,blank=True,null = True)
-    smtp_password = models.CharField(max_length=200,blank=True,null = True)
-    smtp_port = models.CharField(max_length=20,choices=Smtp_Port,default='587',blank=True,null = True)
-    imap_host = models.CharField(max_length=200,blank=True,null = True)
-    imap_username = models.CharField(max_length=200,blank=True,null = True)
-    imap_password = models.CharField(max_length=200,blank=True,null = True)
-    imap_port = models.CharField(max_length=20,choices=Imap_Port,default='993',blank=True,null = True)
-    access_token = models.CharField(max_length=500,blank=True,null=True)
-    provider = models.CharField(max_length=100,choices=Provider,default='Smtp',null=True,blank=True)
+
+    # SMTP fields
+    smtp_host = models.CharField(max_length=200, blank=True, null=True)
+    smtp_port = models.CharField(max_length=20, choices=SMTP_PORTS, default='587', blank=True, null=True)
+    smtp_username = models.CharField(max_length=200, blank=True, null=True)
+    smtp_password = models.CharField(max_length=200, blank=True, null=True)
+
+    imap_host = models.CharField(max_length=200, blank=True, null=True)
+    imap_port = models.CharField(max_length=20, choices=IMAP_PORTS, default='993', blank=True, null=True)
+    imap_username = models.CharField(max_length=200, blank=True, null=True)
+    imap_password = models.CharField(max_length=200, blank=True, null=True)
 
     def __str__(self):
         return self.email
+
+
+class SMTPManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(email_provider='SMTP')
+
+
+class SMTPAccount(EmailAccount):
+    objects = SMTPManager()
+
+    class Meta:
+        proxy = True
+
+
+class GoogleManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(email_provider="Google")
+
+
+class GoogleAccount(EmailAccount):
+    objects = GoogleManager()
+
+    class Meta:
+        proxy = True
+
+
+class MicrosoftManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(email_provider="Microsoft")
+
+
+class MicrosoftAccount(EmailAccount):
+    objects = MicrosoftManager()
+
+    class Meta:
+        proxy = True
+
