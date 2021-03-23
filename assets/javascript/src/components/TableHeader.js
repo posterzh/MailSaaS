f90a7ea2dc1a67046adfdd65c6b16afd4cd04387
Zhang.P <poterasu.zhang@gmail.com>
2021-03-24 00:56:19 +0800

    update campaign details recipients page (UI)

--- assets/javascript/src/components/TableHeader.js
+++ assets/javascript/src/components/TableHeader.js
@@ -1,40 +1,72 @@
 export const campaignListTable = [
     {
-      key: 'title',
-      value: 'Title',
-      link: '/app/admin/campaign/{{id}}/details-overview',
-      id: "id"
+        key: 'title',
+        value: 'Title',
+        link: '/app/admin/campaign/{{id}}/details-overview',
+        id: "id"
     },
     {
-      key: 'created',
-      value: 'CREATED',
+        key: 'created',
+        value: 'CREATED',
     },
     {
-      key: 'assigned',
-      value: 'ASSIGNED',
+        key: 'assigned',
+        value: 'ASSIGNED',
     },
     {
-      key: 'recipients',
-      value: 'RECIPIENTS',
+        key: 'recipients',
+        value: 'RECIPIENTS',
     },
     {
-      key: 'sent',
-      value: 'SENT',
+        key: 'sent',
+        value: 'SENT',
     },
     {
-      key: 'leads',
-      value: 'LEADS',
+        key: 'leads',
+        value: 'LEADS',
     },
     {
-      key: 'replies',
-      value: 'REPLIES',
+        key: 'replies',
+        value: 'REPLIES',
     },
     {
-      key: 'opens',
-      value: 'OPENS',
+        key: 'opens',
+        value: 'OPENS',
     },
     {
-      key: 'bounces',
-      value: 'BOUNCES',
+        key: 'bounces',
+        value: 'BOUNCES',
     }
-  ];
+];
+
+export const recipientsTable = [
+    {
+        key: 'email',
+        value: 'EMAIL',
+        link: '/app/admin/campaign/{{id}}/details-overview',
+        id: "id"
+    },
+    {
+        key: 'name',
+        value: 'NAME',
+    },
+    {
+        key: 'created_date_time',
+        value: 'ADDED ON',
+    },
+    {
+        key: 'updated_date_time',
+        value: 'SEND ON',
+    }
+];
+
+export const recipientsFilters = [
+	{
+		key: 'email',
+		options: []
+	},
+	{
+		key: 'name',
+		options: []
+	}
+];
