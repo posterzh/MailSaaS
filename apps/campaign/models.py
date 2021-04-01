db1ac1e5848141e07ed88df9e6a538d9e6fcb7a4
Zhang.P <poterasu.zhang@gmail.com>
2021-04-02 05:09:17 +0800

    Modify the inbox model.

--- apps/campaign/models.py
+++ apps/campaign/models.py
@@ -154,18 +154,6 @@ class SendingObject(models.Model):
     clicked_datetime = models.DateTimeField(null=True)
 
 
-class EmailInbox(models.Model):
-    recipient_email = models.ForeignKey(EmailAccount, on_delete=models.CASCADE)
-    from_email = models.TextField()
-    email_subject = models.TextField()
-    email_body = models.TextField(blank=True, null=True)
-
-    # 0: received, 1: processed, 2: xxx
-    status = models.PositiveSmallIntegerField(default=0)
-    receive_date = models.DateField(auto_now=False, blank=True, null=True)
-    receive_time = models.TimeField(auto_now=False, blank=True, null=True)
-
-
 class Recipient(models.Model):
     LEAD_TYPE = (
         ("none", "None"),
@@ -224,3 +212,16 @@ class EmailOutbox(models.Model):
     # Email click tracking
     clicked = models.PositiveIntegerField(default=0)
     clicked_datetime = models.DateTimeField(null=True)
+
+
+class EmailInbox(models.Model):
+    recipient_email = models.ForeignKey(Recipient, on_delete=models.SET_NULL, null=True)
+    from_email = models.ForeignKey(EmailAccount, on_delete=models.SET_NULL, null=True)
+    outbox = models.ForeignKey(EmailOutbox, on_delete=models.SET_NULL, null=True)
+    email_subject = models.TextField()
+    email_body = models.TextField(blank=True, null=True)
+
+    # 0: received, 1: processed, 2: other
+    status = models.PositiveSmallIntegerField(default=0)
+    receive_date = models.DateField(auto_now=False, blank=True, null=True)
+    receive_time = models.TimeField(auto_now=False, blank=True, null=True)
