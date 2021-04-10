844f9116be212223d9c0ac5a93fef50294782fb4
Zhang.P <poterasu.zhang@gmail.com>
2021-04-10 14:07:06 +0800

    Karl - Implement Update Profile and Update Password

--- mail/urls.py
+++ mail/urls.py
@@ -42,6 +42,7 @@ urlpatterns = [
 
     path('rest-auth/', include('rest_auth.urls')),
     path('rest-auth/registration/', include('rest_auth.registration.urls')),
+    path(r'^', include('django.contrib.auth.urls')),
 
     path('o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
     path(r'auth/', include('rest_framework_social_oauth2.urls')),
