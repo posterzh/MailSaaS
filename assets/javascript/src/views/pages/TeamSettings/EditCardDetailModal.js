21e6dfe70729b606239b067da0054e93caa33114
Zhang.P <poterasu.zhang@gmail.com>
2021-04-15 03:51:43 +0800

    Add stripe React component.

--- assets/javascript/src/views/pages/TeamSettings/EditCardDetailModal.js
+++ assets/javascript/src/views/pages/TeamSettings/EditCardDetailModal.js
@@ -14,99 +14,41 @@ import {
   CardBody,
 } from "reactstrap";
 import classnames from "classnames";
+import { loadStripe } from "@stripe/stripe-js";
+import {
+  CardElement,
+  Elements,
+  useStripe,
+  useElements,
+} from "@stripe/react-stripe-js";
 
-class EditCardModal extends Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    const { isOpen, handleClose } = this.props;
+const EditCardModal = (props) => {
+  const { isOpen, handleClose } = props;
 
-    return (
-      <Modal isOpen={isOpen}>
-        <Card className="bg-gradient-primary mb-0">
-          <CardBody>
-            <Row className="justify-content-between align-items-center">
-              <Col className="col-auto">
-                <span className="text-white font-weight-bold mr-3">
-                  Edit Card Details
-                </span>
-              </Col>
-            </Row>
-            <div className="mt-4">
-              <Form className="form-primary" role="form">
-                <FormGroup>
-                  <InputGroup
-                    className={classnames("input-group-alternative mb-3")}
-                  >
-                    <InputGroupAddon addonType="prepend">
-                      <InputGroupText>
-                        <i className="ni ni-single-02" />
-                      </InputGroupText>
-                    </InputGroupAddon>
-                    <Input placeholder="Name on card" type="text" />
-                  </InputGroup>
-                </FormGroup>
-                <FormGroup>
-                  <InputGroup
-                    className={classnames("input-group-alternative mb-3")}
-                  >
-                    <InputGroupAddon addonType="prepend">
-                      <InputGroupText>
-                        <i className="ni ni-credit-card" />
-                      </InputGroupText>
-                    </InputGroupAddon>
-                    <Input placeholder="Card number" type="text" />
-                  </InputGroup>
-                </FormGroup>
-                <Row>
-                  <Col xs="6">
-                    <FormGroup>
-                      <InputGroup
-                        className={classnames("input-group-alternative mb-3")}
-                      >
-                        <InputGroupAddon addonType="prepend">
-                          <InputGroupText>
-                            <i className="ni ni-calendar-grid-58" />
-                          </InputGroupText>
-                        </InputGroupAddon>
-                        <Input placeholder="MM/YY" type="text" />
-                      </InputGroup>
-                    </FormGroup>
-                  </Col>
-                  <Col xs="6">
-                    <FormGroup>
-                      <InputGroup
-                        className={classnames("input-group-alternative")}
-                      >
-                        <InputGroupAddon addonType="prepend">
-                          <InputGroupText>
-                            <i className="ni ni-lock-circle-open" />
-                          </InputGroupText>
-                        </InputGroupAddon>
-                        <Input placeholder="CCV" type="text" />
-                      </InputGroup>
-                    </FormGroup>
-                  </Col>
-                </Row>
-                <Button color="success" type="button" className="text-uppercase">
-                  Save Card
-                </Button>
-                <Button
-                  className="text-uppercase ml-xs-0"
-                  onClick={(e) => {
-                    e.preventDefault();
-                    handleClose();
-                  }}
-                >
-                  cancel
-                </Button>
-              </Form>
-            </div>
-          </CardBody>
-        </Card>
-      </Modal>
-    );
-  }
-}
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card: elements.getElement(CardElement),
+    });
+  };
+
+  return (
+    <Modal isOpen={isOpen}>
+      <Card className="bg-gradient-primary mb-0">
+        <CardBody>
+          <form onSubmit={handleSubmit}>
+            <CardElement />
+            <button type="submit" disabled={!stripe}>
+              Save Card
+            </button>
+          </form>
+        </CardBody>
+      </Card>
+    </Modal>
+  );
+};
 export default EditCardModal;
