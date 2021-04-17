6f3d7d7414bb1457de544a422f91409ca14245f8
Zhang.P <poterasu.zhang@gmail.com>
2021-04-18 05:22:31 +0800

    update teammates page for google accounts, input validation and google auth

--- assets/javascript/src/views/pages/examples/Login.js
+++ assets/javascript/src/views/pages/examples/Login.js
@@ -51,6 +51,8 @@ class Login extends React.Component {
 
     const params = new URLSearchParams(props.location.search);
     const email = params.get("email");
+    const invitation_id = params.get("invitation_id");
+    console.log("invitation id: ", invitation_id);
 
     this.state = {
       email: email || "",
@@ -59,6 +61,7 @@ class Login extends React.Component {
       focusedPassword: false,
       loading: false,
       error: false,
+      invitation_id: invitation_id,
     };
   }
 
@@ -103,6 +106,7 @@ class Login extends React.Component {
       email: email,
       first_name: givenName,
       last_name: familyName,
+      invitation_id: this.state.invitation_id,
     };
     const token = response.tokenObj.access_token;
     this.props.googleLogin(user, token);
